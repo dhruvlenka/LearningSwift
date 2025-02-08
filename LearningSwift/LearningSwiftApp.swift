fa48f56e1cd75607feea6dc34abf126f60bb8868
lenkadhruv@gmail.com <lenkadhruv@gmail.com>
2025-02-09 02:02:56 +0530

    Initial Commit

---
+++ LearningSwift/LearningSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftApp.swift
+//  LearningSwift
+//
+//  Created by Dhruv Lenka on 2/9/25.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
